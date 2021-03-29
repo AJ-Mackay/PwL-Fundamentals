82929eb9e5ffb6fa69c65ea863586c51b82c3dfa
Paul Mackay <paul.m.mackay85@gmail.com>
2021-03-29 17:58:25 +0100

    Inserting data

--- example/routes/web.php
+++ example/routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
 //
 //Route::get('/about', function () {
 //    return "Hello from the about page!";
@@ -38,6 +38,11 @@ use Illuminate\Support\Facades\Route;
 
 //Route::resource('posts', 'App\Http\Controllers\PostsController');
 
-Route::get('/contact', 'App\Http\Controllers\PostsController@contact');
+//Route::get('/contact', 'App\Http\Controllers\PostsController@contact');
 
-Route::get('/post/{id}/{name}/{password}', 'App\Http\Controllers\PostsController@show_post');
+//Route::get('/post/{id}/{name}/{password}', 'App\Http\Controllers\PostsController@show_post');
+
+//
+Route::get('/insert', function(){
+    DB::insert('insert into posts(title, content) values(?, ?)', ['PHP with Laravel', 'Laravel is the best thing that has happened to PHP']);
+});
