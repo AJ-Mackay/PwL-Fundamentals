c068758e623bbacf357139d037dbbf783a0eecfd
Paul Mackay <paul.m.mackay85@gmail.com>
2021-04-12 11:13:00 +0100

    Setting the markup for create

---
+++ example/resources/views/posts/create.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+
+<form method="post" action="/posts">
+    <input type="text" name="title" placeholder="Enter title">
+    <input type="submit" name="submit">
+</form>
