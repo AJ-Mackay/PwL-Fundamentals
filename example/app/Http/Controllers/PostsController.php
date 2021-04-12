8294380130c3ad4bf7f3415daebd214fb109a93a
Paul Mackay <paul.m.mackay85@gmail.com>
2021-04-12 21:57:07 +0100

    Editing records and redirecting

--- example/app/Http/Controllers/PostsController.php
+++ example/app/Http/Controllers/PostsController.php
@@ -86,7 +86,11 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $post = Post::findOrFail($id);
+
+        $post->update($request->all());
+
+        return redirect('posts');
     }
 
     /**
