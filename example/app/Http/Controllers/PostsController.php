a500ca8b0f2cd872773336edff9245c8048da439
Paul Mackay <paul.m.mackay85@gmail.com>
2021-04-12 22:06:25 +0100

    Deleting

--- example/app/Http/Controllers/PostsController.php
+++ example/app/Http/Controllers/PostsController.php
@@ -101,7 +101,9 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::whereId($id)->delete();
+
+        return redirect('posts');
     }
 
     public function contact()
--- example/resources/views/posts/edit.blade.php
+++ example/resources/views/posts/edit.blade.php
@@ -8,7 +8,13 @@
     @csrf
     <input type="hidden" name="_method" value="PUT">
     <input type="text" name="title" placeholder="Enter title" value="{{$post->title}}">
-    <input type="submit" name="submit">
+    <input type="submit" name="submit" value="UPDATE">
+</form>
+
+<form method="post" action="/posts/{{$post->id}}">
+    @csrf
+    <input type="hidden" name="_method" value="DELETE">
+    <input type="submit" name="delete" value="DELETE">
 </form>
 
 @endsection
