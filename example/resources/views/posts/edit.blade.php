28feb83a8537262eb8a5a264d5220e780046f6cb
Paul Mackay <paul.m.mackay85@gmail.com>
2021-04-12 21:50:43 +0100

    Preparing the edit view and setting up the token

--- example/resources/views/posts/edit.blade.php
+++ example/resources/views/posts/edit.blade.php
@@ -4,9 +4,10 @@
 
 <h1>Edit Post</h1>
 
-<form method="post" action="/posts">
+<form method="post" action="/posts/{{$post->id}}">
     @csrf
-    <input type="text" name="title" placeholder="Enter title">
+    <input type="hidden" name="_method" value="PUT">
+    <input type="text" name="title" placeholder="Enter title" value="{{$post->title}}">
     <input type="submit" name="submit">
 </form>
 
