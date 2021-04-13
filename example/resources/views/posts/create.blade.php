4ac808fefc7d4b8043191fc1fb4d47bd72a4cc26
Paul Mackay <paul.m.mackay85@gmail.com>
2021-04-13 11:46:48 +0100

    Modifying our create form with the form package

--- example/resources/views/posts/create.blade.php
+++ example/resources/views/posts/create.blade.php
@@ -3,11 +3,15 @@
 @section('content')
 
 <h1>Create Post</h1>
-{!! Form::open() !!}
-<!--<form method="post" action="/posts">-->
+{!! Form::open(['method'=>'POST', 'action'=>'App\Http\Controllers\PostsController@store']) !!}
     @csrf
-    <input type="text" name="title" placeholder="Enter title">
-    <input type="submit" name="submit">
-</form>
+    <div class="form-group">
+        {!! Form::label('title', 'Title:') !!}
+        {!! Form::text('title', null, ['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::submit('Create Post', ['class'=>'btn-primary']) !!}
+    </div>
+{!! Form::close() !!}
 
 @endsection
