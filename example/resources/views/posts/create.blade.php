b61bdbedfe8de9b33723aa1cb475ca240e9a313a
Paul Mackay <paul.m.mackay85@gmail.com>
2021-04-13 11:19:03 +0100

    Installing package and testing

--- example/resources/views/posts/create.blade.php
+++ example/resources/views/posts/create.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
 <h1>Create Post</h1>
-
-<form method="post" action="/posts">
+{!! Form::open() !!}
+<!--<form method="post" action="/posts">-->
     @csrf
     <input type="text" name="title" placeholder="Enter title">
     <input type="submit" name="submit">
