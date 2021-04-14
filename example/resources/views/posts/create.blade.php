a0da013a32428340854dc6d8503f435ed4046cd1
Paul Mackay <paul.m.mackay85@gmail.com>
2021-04-14 14:50:03 +0100

    Modifying our create view for file input

--- example/resources/views/posts/create.blade.php
+++ example/resources/views/posts/create.blade.php
@@ -3,11 +3,14 @@
 @section('content')
 
 <h1>Create Post</h1>
-{!! Form::open(['method'=>'POST', 'action'=>'App\Http\Controllers\PostsController@store']) !!}
+{!! Form::open(['method'=>'POST', 'action'=>'App\Http\Controllers\PostsController@store', 'files'=>true]) !!}
     @csrf
     <div class="form-group">
         {!! Form::label('title', 'Title:') !!}
         {!! Form::text('title', null, ['class'=>'form-control']) !!}
+    </div>
+    <div class='form-group'>
+    {!! Form::file('file', ['class'=>'form-control']) !!}
     </div>
     <div class="form-group">
         {!! Form::submit('Create Post', ['class'=>'btn btn-primary']) !!}
